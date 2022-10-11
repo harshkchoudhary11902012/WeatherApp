f664a17060da1a36758520879b7be5c062d59136
Harsh Kumar Choudhary <choudharyharshofficial@gmail.com>
2022-10-11 19:43:53 +0530

    Add files via upload

---
+++ request.js
@@ -0,0 +1,32 @@
+//const key = '4ee5b7893430866bb578c61c7a780ba3';
+//const baseURL='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=4ee5b7893430866bb578c61c7a780ba3';
+
+//fetch(baseURL).then((data) => { console.log('response', data)} )
+
+
+
+
+const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
+
+// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+
+// fetch(baseURL)
+//     .then((data) => { console.log('response', data.json()) })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
+
+requestCity('Jalandhar');
